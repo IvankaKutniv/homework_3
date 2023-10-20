e80428b57420bc36769a891c7320af501d0a6cab
ivanka.kutniv <bahb4uku@gmail.com>
2023-10-20 21:48:27 +0300

    add HW myFunctions

---
+++ myFunctions/myFunctionsHW/script.js
@@ -0,0 +1,49 @@
+"use strict";
+//ДЗ 1: Реалізація функції isNaN
+
+/*function isNaN(num) {
+    console.log(num);
+    let result = num**2 / 3;
+    return result;
+}
+console.log(isNaN(36));*/
+
+
+//ДЗ 2: Реалізація функцій padEnd та padStart
+
+/*function pad(myRow, sign, amount, boolean) {
+        if (boolean == true)
+            return myRow.padStart(amount, sign);
+        else
+            return myRow.padEnd(amount, sign);
+}
+console.log(pad('123.45', '$', 7, true));*/
+
+
+//ДЗ 3: Перевірка теорії ймовірності
+
+/*function checkProbabilityTheory(count) {
+    let min = 100;
+    let max = 1000;
+    let paired = 0;
+    let unpaired = 0;
+    for (let i = 0; i < count; i++) {
+        let rnd = Math.random() * (max - min) + min;
+        rnd = Math.ceil(rnd);
+        console.log(rnd);
+        if (rnd % 2) {
+            paired += 1;
+        } else {
+            unpaired += 1;
+        }
+    }
+    console.log('Кількість згенерованих чисел: ' + count);
+    console.log('Парних чисел: ' + paired);
+    console.log('Непарних чисел: ' + unpaired);
+    let countPaired = (paired / count) * 100;
+    let countUnpaired = (unpaired / count) * 100;
+    console.log('Відсоток парних до непарних: ' + countPaired + '%' + ' / ' + countUnpaired + '%');
+}
+checkProbabilityTheory(10);*/
+
+
